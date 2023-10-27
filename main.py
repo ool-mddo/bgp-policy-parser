e658224bde09dbfe877e054c6b366f999de8844c
Daiki Yamaguchi <daiki.yamaguchi.069@ctc-g.co.jp>
2023-10-27 15:02:03 +0900

    prefix-length-range,min<->max 修正

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def parse_juniper_bgp_policy(network: str, snapshot: str) -> None:
                         elif len(match_type_elem) == 2:
                             if match_type_elem[0] == "prefix-length-range":
                                 # ex. prefix-length-range /25-/27 -> {"min": 25, "max": 27}
-                                max_length, min_length = [
+                                min_length, max_length = [
                                     x.lstrip("/") for x in match_type_elem[1].split("-")
                                 ]
                                 length = {"max": max_length, "min": min_length}
